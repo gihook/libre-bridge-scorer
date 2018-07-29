d292f456cbd6500238f6e504392e958cae2d1591
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2018-07-29 21:06:10 +0200

    Added configuration for debugging node application in VSCode First working version for creating tournament with all board entries

--- node-app/services/tournament-service.js
+++ node-app/services/tournament-service.js
@@ -1,13 +1,37 @@
 const tournamentRepository = require('../data-access/repositories/tournament-repository');
 const tournamentDefinition = require('../data/tournament-definitions/howell4-28.json');
+const _ = require('underscore');
 
 const createNewTournament = data => {
-    let numberOfBoards = data.numberOfBoards;
+    const numberOfBoards = data.numberOfBoards;
     data.boards = [];
+    data.results = [];
     for (let i = 1; i <= numberOfBoards; i++)
         data.boards.push({ boardNumber: i });
 
+    for (let table of tournamentDefinition.tables) {
+        for (let round of table.rounds) {
+            const boardNumbers = getBoardNumbers(round.boards);
+            for (const boardNumber of boardNumbers) {
+                data.results.push({
+                    boardNumber: boardNumber,
+                    nsPair: round.ns,
+                    ewPair: round.ew,
+                });
+            }
+        }
+    }
+
     return tournamentRepository.saveTournament(data);
+}
+
+const getBoardNumbers = boardRange => {
+    const regex = new RegExp('(.*)-(.*)'); // TODO make more precise regex
+    const matches = regex.exec(boardRange);
+    const lower = matches[1];
+    const upper = matches[2];
+
+    return _.range(Number(lower), Number(upper) + 1);
 }
 
 const getTournamentById = id => {
